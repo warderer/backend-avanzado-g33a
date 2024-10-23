31b2249cdd115862b16ef8a0059c511945974710
César Guerra <warderer@gmail.com>
2024-10-22 21:39:12 -0600

    ✨ Add carRoutes

--- 02-cars-api/index.js
+++ 02-cars-api/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import { connect } from './config/database.js'
+import carRoutes from './routes/carRoutes.js'
 
 const PORT = process.env.PORT || 3000
 
@@ -8,6 +9,7 @@ const api = express()
 api.use(express.json()) // Parsear el body de las peticiones a JSON
 
 // Aquí van las rutas
+api.use('/api/v1', carRoutes)
 
 // Nos conectamos a la base de datos y luego levantamos el servidor
 connect().then(() => {
