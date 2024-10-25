bf7c5f843ec3dded111a3d2d6a365acd9fce3214
César Guerra <warderer@gmail.com>
2024-10-24 20:02:24 -0600

    ✨ Add deleteCarById endpoint and update getAllCars, getCarById for softdelete

--- 02-cars-api/controllers/carController.js
+++ 02-cars-api/controllers/carController.js
@@ -15,7 +15,7 @@ const createCar = async (req, res) => {
 // Get All Cars
 const getAllCars = async (req, res) => {
   try {
-    const cars = await Car.find()
+    const cars = await Car.find({ isActive: true })
     // const cars = await Car.find({}, { model: 1, brand: 1 }) // Con proyecciones
     res.status(200).json(cars)
   } catch (error) {
@@ -26,7 +26,7 @@ const getAllCars = async (req, res) => {
 // Get Car by ID
 const getCarById = async (req, res) => {
   try {
-    const car = await Car.findById(req.params.carId)
+    const car = await Car.find({ _id: req.params.carId, isActive: true })
     res.status(200).json(car)
   } catch (error) {
     res.status(400).json({ message: 'Error Getting Car', error })
@@ -45,10 +45,36 @@ const updateCarById = async (req, res) => {
 }
 
 // Delete
+const deleteCarById = async (req, res) => {
+  // Borrado físico: Voy a comprobar si existe un query string llamado 'destroy' y si su valor es 'true' voy a borrar el resitro de la base de datos. ?destroy=true
+  if (req.query.destroy === 'true') {
+    try {
+      const deletedCar = await Car.findByIdAndDelete(req.params.carId)
+      if (deletedCar === null) { // Valido si el carro NO existe
+        return res.status(404).json({ message: 'Car not found for Delete' })
+      }
+      return res.status(204).json()
+    } catch (error) {
+      return res.status(400).json({ message: 'Error Deleting Car', error })
+    }
+  }
+
+  // Borrado lógico: Cambio el estado de isActive a false (Update -> findByIdAndUpdate())
+  try {
+    const updatedCar = await Car.findByIdAndUpdate(req.params.carId, { isActive: false }, { new: false })
+    if (updatedCar === null || updatedCar.isActive === false) { // Valido si el carro NO existe o ya está desactivado
+      return res.status(404).json({ message: 'Car not found for Delete' })
+    }
+    return res.status(204).json()
+  } catch (error) {
+    return res.status(400).json({ message: 'Error Deleting Car', error })
+  }
+}
 
 export {
   createCar,
   getAllCars,
   getCarById,
-  updateCarById
+  updateCarById,
+  deleteCarById
 }
