89b4e665062b692bc47bbc29b11fd9b957ca3ef7
César Guerra <warderer@gmail.com>
2024-10-31 21:13:22 -0600

    ✨ Add User Model

---
+++ 03-books-api/models/User.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+  first_name: {
+    type: String,
+    required: true
+  },
+  last_name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ['ADMIN', 'USER'],
+    default: 'USER'
+  }
+}, { timestamps: true }) // createdAt, updatedAt
+
+export default mongoose.model('User', userSchema)
