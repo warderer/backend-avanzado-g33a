951dc2816391fac6f76333fea8c56331a6302298
César Guerra <warderer@gmail.com>
2024-10-31 20:55:23 -0600

    ✨ Add Morgan (tiny) http logger to server

--- 03-books-api/index.js
+++ 03-books-api/index.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import { connect } from './config/database.js'
 import bookRoutes from './routes/bookRoutes.js'
+import morgan from 'morgan'
 
 const PORT = process.env.PORT || 3000
 
 const app = express()
 app.use(express.json())
+app.use(morgan('tiny'))
 
 // Aquí van las rutas
 app.use('/api/v1/books', bookRoutes)
