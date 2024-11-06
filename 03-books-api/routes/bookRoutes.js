871e9bfaa31d6d4e27fee81f86eb32fa7077a752
César Guerra <warderer@gmail.com>
2024-11-05 21:28:11 -0600

    ✨ Add isAuth and isAdmin middlewares to bookRoutes

--- 03-books-api/routes/bookRoutes.js
+++ 03-books-api/routes/bookRoutes.js
@@ -1,13 +1,15 @@
 import express from 'express'
 import { createBook, deleteBookById, getAllBooks, getBookById, updateBookById } from '../controllers/bookController.js'
+import { isAuth } from '../middlewares/isAuth.js'
+import { isAdmin } from '../middlewares/isAdmin.js'
 
 const bookRoutes = express.Router()
 
 // Rutas de libros
-bookRoutes.post('/', createBook)
+bookRoutes.post('/', isAuth, isAdmin, createBook)
 bookRoutes.get('/', getAllBooks)
 bookRoutes.get('/:bookId', getBookById)
-bookRoutes.patch('/:bookId', updateBookById)
-bookRoutes.delete('/:bookId', deleteBookById)
+bookRoutes.patch('/:bookId', isAuth, isAdmin, updateBookById)
+bookRoutes.delete('/:bookId', isAuth, isAdmin, deleteBookById)
 
 export default bookRoutes
